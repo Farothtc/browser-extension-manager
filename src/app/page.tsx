acd96110ec0c58923bca093aa8974f95dd31ca54
Farothtc <mvkirimi@gmail.com>
2025-05-14 16:45:00 +0300

    feat: general page structure improved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Nav from "./Components/Nav";
 
 export default function Home() {
+  const style = {
+    backgroundImage: "linear-gradient(#04091c,#0a1542)",
+    backgroundSize: "cover",
+    backgroundPosition: "contain",
+  };
   return (
-    <main className="min-h-screen">
-      <h1>Helo</h1>
+    <main className="min-h-screen" style={style}>
+      <section className="container mx-auto pt-6">
+        <Nav />
+        <h1>Helo</h1>
+      </section>
     </main>
   );
 }
