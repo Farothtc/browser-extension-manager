d07deaa59c75a96e92da0652a54d2bf6bb0497b4
Farothtc <mvkirimi@gmail.com>
2025-05-15 18:46:32 +0300

    feat: responsivity improved

--- src/app/Components/Extensions.tsx
+++ src/app/Components/Extensions.tsx
@@ -12,7 +12,7 @@ export default function Extensions({
 }: ExtensionsProps) {
   return (
     <>
-      <div className="grid col-span-1 row-span-1 bg-[#1f2535] border border-gray-500 rounded-3xl">
+      <div className="grid col-span-1 row-span-1 bg-[#1f2535] border border-gray-500 rounded-3xl dark:bg-[#fcfdff] dark:border-0 dark:shadow-sm">
         <div className="grid grid-cols-1 grid-rows-1">
           <div className="grid col-span-1 row-span-1">
             <div className="card card-md">
@@ -26,15 +26,17 @@ export default function Extensions({
                     className="pb-8"
                   ></Image>
                   <div className="flex flex-col">
-                    <h2 className="card-title font-bold text-xl">{e.name}</h2>
-                    <p className="text-lg font-medium text-gray-400">
+                    <h2 className="card-title font-bold text-xl dark:text-black">
+                      {e.name}
+                    </h2>
+                    <p className="text-lg font-medium text-gray-500">
                       {e.description}
                     </p>
                   </div>
                 </div>
                 <div className="justify-between card-actions items-center pt-10">
                   <button
-                    className="btn active:bg-red-500 active:text-black active:border-black bg-[#1f2535] text-white border-gray-500 rounded-3xl"
+                    className="btn active:bg-red-500 active:text-black active:border-black bg-[#1f2535] text-white border-gray-500 rounded-3xl dark:bg-[#feffff] dark:text-black dark:hover:border-red-500"
                     onClick={handleRemove}
                   >
                     Remove
@@ -43,7 +45,7 @@ export default function Extensions({
                     type="checkbox"
                     checked={e.isActive}
                     onChange={onToggle}
-                    className="toggle text-white border-gray-500 checked:border-red-700 checked:bg-red-500 checked:text-white"
+                    className="toggle text-white border-gray-500 checked:border-red-900 checked:bg-red-600 checked:text-white dark:bg-[#c6c6c8] dark:hover:border-red-500"
                   />
                 </div>
               </div>
--- src/app/Components/Nav.tsx
+++ src/app/Components/Nav.tsx
@@ -1,27 +1,45 @@
 import Image from "next/image";
-export default function Nav() {
-  const style = {
-    backgroundColor: "#1f2535",
-  };
+import { NavProps } from "../types";
+
+export default function Nav({ isDark, handleThemeToggle }: NavProps) {
   return (
     <>
-      <div
-        className="navbar shadow-sm rounded-2xl flex justify-between"
-        style={style}
-      >
-        <Image
-          src="/images/logo.svg"
-          alt="Logo"
-          width={200}
-          height={100}
-        ></Image>
-        <button className="btn btn-ghost rounded-xl w-[50px] h-[50px] p-0 bg-[#2f354d]">
+      <div className="navbar shadow-sm rounded-2xl flex justify-between bg-[#1f2535] dark:bg-[#fcfdff]">
+        {isDark ? (
           <Image
-            src="/images/icon-sun.svg"
-            alt="Sun"
-            width={25}
-            height={25}
+            src="/images/logo-dark.svg"
+            alt="Logo"
+            width={200}
+            height={100}
           ></Image>
+        ) : (
+          <Image
+            src="/images/logo.svg"
+            alt="Logo"
+            width={200}
+            height={100}
+          ></Image>
+        )}
+
+        <button
+          className="btn btn-ghost rounded-xl w-[50px] h-[50px] p-0 bg-[#2f354d] dark:bg-[#eeeeed] hover:border-red-500 dark:hover:border-red-500"
+          onClick={handleThemeToggle}
+        >
+          {isDark ? (
+            <Image
+              src="/images/icon-moon.svg"
+              alt="Moon"
+              width={25}
+              height={25}
+            ></Image>
+          ) : (
+            <Image
+              src="/images/icon-sun.svg"
+              alt="Sun"
+              width={25}
+              height={25}
+            ></Image>
+          )}
         </button>
       </div>
     </>
